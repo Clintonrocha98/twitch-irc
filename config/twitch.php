f35fb20f7f350988c12b4f590fcffabb7f2af3b3
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-12-12 22:10:15 -0300

    feat: add Twitch configuration file for IRC connection settings

---
+++ config/twitch.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'irc' => [
+        'server' => env('TWITCH_IRC_SERVER', 'irc.chat.twitch.tv'),
+        'port'   => env('TWITCH_IRC_PORT', 6697),
+        'token'  => env('TWITCH_IRC_TOKEN'),
+        'nick'   => env('TWITCH_IRC_NICK'),
+        'channel'=> env('TWITCH_IRC_CHANNEL'),
+    ],
+];
