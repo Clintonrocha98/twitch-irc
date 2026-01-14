ff8a3cbe9d4e791579ef24de9d5238812af40912
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-01-14 19:05:56 -0300

    feat: add RawMessageParser class for parsing raw IRC messages

---
+++ app-modules/twitch-irc/src/Parser/RawMessageParser.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ClintonRocha\TwitchIrc\Parser;
+
+class RawMessageParser
+{
+    public function parse(string $line): RawMessage
+    {
+        $raw = new RawMessage;
+
+        if (str_starts_with($line, '@')) {
+            [$tagPart, $line] = explode(' ', $line, 2);
+            $raw->rawTags = mb_substr($tagPart, 1);
+        }
+
+        if (str_starts_with($line, ':')) {
+            [$prefix, $line] = explode(' ', $line, 2);
+            $raw->prefix = mb_substr($prefix, 1);
+        }
+
+        if (str_contains($line, ' :')) {
+            [$header, $text] = explode(' :', $line, 2);
+            $raw->text = $text;
+        } else {
+            $header = $line;
+        }
+
+        $parts = explode(' ', $header);
+        $raw->command = array_shift($parts);
+        $raw->params = $parts;
+
+        return $raw;
+    }
+}
